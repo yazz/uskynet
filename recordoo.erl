b58e20bdf3403b947fe8b41d927f24f50b819670
Zubair Quraishi <zubairq@gmail.com>
2010-04-19 18:19:49 +0200

    Added better OO help and ability to create OO records

--- recordoo.erl
+++ recordoo.erl
@@ -6,3 +6,5 @@ get_property_names( ) -> zql:get_property_names( Conn, Key ).
 print( ) -> zql:print( Conn, Key).
 
 get(PropertyName) -> zql:get_property( Conn, Key , PropertyName).
+
+set(PropertyName, Value) -> zql:set_property(Conn, Key, PropertyName, Value).
--- zqloo.erl
+++ zqloo.erl
@@ -1,5 +1,40 @@
 -module(zqloo,[Conn]).
 -compile(export_all).
+-import(zprint,[println/1,p/1,q/1,print_number/1]).
+-import(zutils,[uuid/0]).
+
+help() -> 
+p("-----------------------------------------------------------------------------"),
+p("-                                                                           -"),
+p("-                                MODULE zqloo                               -"),
+p("-                                                                           -"),
+p("-                 OO Erlang interface to the ZQL system                     -"),
+p("-                                                                           -"),
+p("-----------------------------------------------------------------------------"),
+p("                                                                             "),
+p("help (Command)                                   get help on a command       "),
+p("test ()                                          run the self tests for zql  "),
+p("                                                                             "),
+p("ls ()                                                                        "),
+p("set (Key, Value)                                 set a value                 "),
+p("put (Key, Value)                                 set a value                 "),
+p("get (Key)                                        get the value of Key        "),
+p("get_record( Key )                                get the record stored at Key"),
+p("exists (Key)                                     does this record/Key exist  "),
+p("                                                                             "),
+p("create_record ( )                                create and return a record  "),
+p("delete_record ( Key )                            delete a record             "),
+p("                                                                             "),
+p("print_all ( )                                    print all records           "),
+p("count( )                                         count the number of records "),
+p("                                                                             "),
+p(" Example:                                                                    "),
+p("C = [{driver,db_riak_driver}, {hostname,'riak@127.0.0.1'},{bucket,<<\"default\">>}]."),
+p("DB = zql:session(C).                                                         "),
+q('DB:set("Name", "Scott").                                                     '),
+q('DB:get("Name" ).                                                             '),
+p("-----------------------------------------------------------------------------"),
+ok.
 
 get(Key) -> zql:get(Conn,Key).
 set(Key,Value) -> zql:set(Conn, Key,Value).
@@ -9,3 +44,7 @@ count( ) -> zql:count(Conn).
 
 
 get_record(Key) -> recordoo:new(Conn,Key).
+
+create_record( ) -> Key = zql:create_record( Conn ),
+                    Record = recordoo:new( Conn, Key ),
+                    Record.
