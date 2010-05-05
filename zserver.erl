8dfd04295ae95666d1d5cac6dac83f2ee41f02ea
Zubair Quraishi <zubairq@gmail.com>
2010-05-05 18:27:32 +0200

    added zserver start and stop

--- zserver.erl
+++ zserver.erl
@@ -3,20 +3,40 @@
 
 -import(zprint,[println/1,p/1,q/1,print_number/1]).
 
-start() -> loop2().
+start() -> Location = global:whereis_name(server),
+           case Location of
+                undefined -> Pid = spawn(zserver,loop2,[]),
+                             global:register_name(server,Pid),
+                             p("Server started");
 
-loop2() ->
-            p("called"),
-            timer:sleep(5000),
+                _ ->         p("Server already started")
+           end.
+
+
+stop() -> Location = global:whereis_name(server),
+           case Location of
+
+                undefined -> p("Server already stopped");
+
+                Pid -> erlang:exit(Pid,kill),
+                p("Stopped")
+           end.
+
+
+loop2() ->  U=user_default,
+            lists:foreach(fun(X) -> zprint:p(X) end, U:lsdb()),
+%            p(X),
+            timer:sleep(15000),
             loop2().
 
 loop() ->
 
   receive
-      {From, Message} -> 
-         io:format("Received: ~w~n",[Message]),
+      start -> 
+         p("starting server"),
          loop();
       stop ->
+         p("Stopping server"),
          true
 
   end.
