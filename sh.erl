99521ae9a8b4bd6312c99b680d80027e973124fa
Zubair Quraishi <zubairq@gmail.com>
2010-05-12 10:11:30 +0200

    more shell

--- sh.erl
+++ sh.erl
@@ -3,7 +3,7 @@
 -import(zprint,[println/1,p/1,q/1,print_number/1]).
 -import(zutils,[uuid/0]).
 
-sh() -> 
+sh() -> p("------------------------------------"),
         Input = io:get_line(">"),
         
         Tokens = string:tokens(Input, " \n"),
@@ -11,14 +11,19 @@ sh() ->
 
 
         case Command of
+             "it" -> it(),sh();
              "help" -> help(),sh();
              "quit" -> finished;
-             X -> store(Input)
+             X -> store(Input),sh()
         end.
 
 help() -> 
 p("help - this command"),
+p("it - the last thing created"),
 p("quit - exits to the command line").
 
 store(Text) -> 
                user_default:store(Text).
+
+it() -> R = user_default:last(),
+        R:print().
